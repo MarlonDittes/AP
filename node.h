c3aa01be3250b89e8e7a8727847a4346e9672e8a
MarlonDittes <marlon.dittes@dittes.de>
2023-07-04 13:32:53 +0200

    Quick fix for heap

--- node.h
+++ node.h
@@ -10,10 +10,7 @@ class Node{
         bool visited = false;
         bool boundary = false;
         int partition = -1;
-
-        //Heap Stuff
         int heapIndex = -1;
-        double heapPriority = -1;
 
     public:
         Node():
@@ -34,12 +31,8 @@ class Node{
         void setPartition (int p) { this->partition = p; };
         int getPartition () { return this->partition; };
 
-        //Heap Stuff
         void setHeapIndex (int h) { this->heapIndex = h; };
         int getHeapIndex () { return this->heapIndex; };
-
-        void setHeapPriority (double p) { this->heapPriority = p; };
-        double getHeapPriority () { return this->heapPriority; };
 
         bool operator==(const Node& other) const {
             return index == other.index;
