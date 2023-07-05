61a1684207d43c9e098a5debd6dc94b4a20238f0
MarlonDittes <marlon.dittes@dittes.de>
2023-07-05 18:14:36 +0200

    Single Edge with Pointer system

--- edge.h
+++ edge.h
@@ -14,9 +14,33 @@ class Edge{
 
         int source;
         int destination;
-        int backwardEIndex = -1;
         double distance;
-        std::vector<bool> arcFlags;
+        std::vector<bool> arcFlagsForwards;
+        std::vector<bool> arcFlagsBackwards;
+
+        int getDestination(int n){
+            if (n == source){
+                return destination;
+            } else {
+                return source;
+            }
+        }
+
+        bool getArcFlag(int n, int k){
+            if (n == source){
+                return arcFlagsForwards[k];
+            } else {
+                return arcFlagsBackwards[k];
+            }
+        }
+
+        void setBackwardArcFlag(int n, int k){
+            if (n == source){
+                arcFlagsForwards[k] = 1;
+            } else {
+                arcFlagsBackwards[k] = 1;
+            }
+        }
 };
 
 #endif
