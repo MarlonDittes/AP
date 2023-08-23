2d12cd7cef0679f4b3425fdeee96bd5fc8eacb83
MarlonDittes <mali.dittes@gmail.com>
2023-08-23 16:22:19 +0200

    No plot mode

--- main.cpp
+++ main.cpp
@@ -32,7 +32,7 @@ int mymain(int source, int target, std::string graphfile, std::string testfile, 
     readPartitionFile(partitionfile, nodeArray);
 
     // get bottom left to top right
-    int bottomLeft = 0;
+    /*int bottomLeft = 0;
     int topRight = 0;
     for (int i = 0; i < nodeArray.size(); i++){
         if (nodeArray[i].getX() <= nodeArray[bottomLeft].getX() && nodeArray[i].getY() <= nodeArray[bottomLeft].getY()){
@@ -43,7 +43,7 @@ int mymain(int source, int target, std::string graphfile, std::string testfile, 
         }
     }
     source = bottomLeft + 1;
-    target = topRight + 1;
+    target = topRight + 1;*/
 
     //Start preprocessing with ArcFlags and timing it
     auto preProcStart = std::chrono::high_resolution_clock::now();
